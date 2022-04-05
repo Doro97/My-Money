277976e3b751740ec4973c07f1cff80f9b1c4927
dorothy <dorothycheruiyot97@gmail.com>
2022-04-05 11:14:36 +0300

    landing page

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyMoney| Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="select" style="width: 200px; ">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Select Month</option>
+                <option value="Jan">January</option>
+                <option value="Feb">February</option>
+                <option value="Mar">March</option>
+                <option value="Apr">April</option>
+                <option value="May">May</option>
+                <option value="Jun">June</option>
+                <option value="Jul">July</option>
+                <option value="Aug">August</option>
+                <option value="Sept">September</option>
+                <option value="Oct">October</option>
+                <option value="Nov">November</option>
+                <option value="Dec">December</option>
+            </select>
+
+        </div>
+       
+        <form action="index.php" method="POST">
+            <div class="form-group">
+              <label for="inputAllocate">ALLOCATE</label>
+              <input type="text" class="form-control"  name="allocate" placeholder="Enter initial investment amount">     
+            </div>
+            <div class="form-group">
+                <label for="inputSIP">SIP</label>
+                <input type="text" class="form-control" name="sip" placeholder="Enter monthly SIP payments ">     
+            </div>
+            <div class="form-group">
+                <label for="inputChange">CHANGE</label>
+                <input type="text" class="form-control" name="change" placeholder="Enter monthly rate of change">     
+            </div>       
+            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+          </form>            
+    </div>
+
+ <?php
+$allocate=$_POST["allocate"];
+$sip=$_POST["sip"];
+$change=$_POST["change"];
+
+if(isset($_POST['submit'])){
+    $result=($allocate+$sip)*(1+$change);
+    echo "Balance: ", $result;
+
+}
+
+?>
+    
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>      
+
+</body>
+</html>
