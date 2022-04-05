fc0fc37c6092a9fd284282a4ceb1926f22d637c9
dorothy <dorothycheruiyot97@gmail.com>
2022-04-05 22:20:41 +0300

    post values

--- processing.php
+++ processing.php
@@ -1,4 +1,5 @@
 <?php
+if(isset($_POST['submit'])){
     $eallocate=$_POST["eallocate"];
     $esip=$_POST["esip"];
     $echange=$_POST["echange"];
@@ -9,11 +10,10 @@
     $gsip=$_POST["gsip"];
     $gchange=$_POST["gchange"];
 
-    if(isset($_POST['submit'])){
-        $eresult=($eallocate+$esip)*(1+$echange);
-        $dresult=($dallocate+$dsip)*(1+$dchange);
-        $gresult=($gallocate+$gsip)*(1+$gchange);
-        echo "Balance: ", $eresult, "\t" ,$dresult,"\t", $gresult;
+    $eresult=($eallocate+$esip)*(1+$echange);
+    $dresult=($dallocate+$dsip)*(1+$dchange);
+    $gresult=($gallocate+$gsip)*(1+$gchange);
+    echo "BALANCE: ", $eresult, "\t", $dresult, "\t", $gresult;
 }
 
 ?>
